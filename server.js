a48a5354d23fb01d2524bc220d057c7ca1334a48
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-04-19 14:54:38 -0700

    set up file structure

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 app.listen(3000, () => {
   console.log('App is listening on 3000');
 })
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
